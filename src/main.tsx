39cdc16c326a60b9a314518e7fce413baf2fac99
sofiane <djebbsoso@gmail.com>
2025-04-27 19:50:00 +0100

    add switch dark mode color

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import theme from "./theme";
-// import { ColorModeButton, ColorModeProvider } from "./components/ui/color-mode";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      {/* <ColorModeProvider /> */}
       <App />
     </ChakraProvider>
   </React.StrictMode>
