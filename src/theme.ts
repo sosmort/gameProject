ba6686938944ade9c3c6ffd4c62cc5ca41325c09
sofiane <djebbsoso@gmail.com>
2025-04-27 19:32:07 +0100

    add dark theme

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+// themeConfig.ts
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
+export default theme;
