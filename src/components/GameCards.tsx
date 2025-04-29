92e7b3d26f36b10cd72f773a8e899d8c9d209df8
sofiane <djebbsoso@gmail.com>
2025-04-30 00:24:32 +0100

    feat: displaying platform icons

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Image, Heading } from "@chakra-ui/react";
 import { Game } from "./hooks/useGames";
+import PlatformIcons from "./PlatformIcons";
 interface Props {
   game: Game;
 }
@@ -9,6 +10,9 @@ const GameCards = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name} </Heading>
+        <PlatformIcons
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
--- src/components/hooks/useGames.ts
+++ src/components/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import apiClient from "../../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 interface FetchGamesResonse {
   count: number;
@@ -24,7 +31,7 @@ const useGames = () => {
         setErrors(err.message);
       });
     return () => controller.abort();
-  });
+  }, []);
   return { games, errors };
 };
 
