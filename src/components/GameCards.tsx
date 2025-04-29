87e0e33111b9d1acc463b701b782fe44f446e073
sofiane <djebbsoso@gmail.com>
2025-04-29 23:38:34 +0100

    building game cards

---
+++ src/components/GameCards.tsx
@@ -0,0 +1,17 @@
+import { Card, CardBody, Image, Heading } from "@chakra-ui/react";
+import { Game } from "./hooks/useGames";
+interface Props {
+  game: Game;
+}
+const GameCards = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name} </Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCards;
--- src/components/hooks/useGames.ts
+++ src/components/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import apiClient from "../../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 interface FetchGamesResonse {
   count: number;
@@ -16,7 +17,7 @@ const useGames = () => {
   useEffect(() => {
     const controller = new AbortController();
     apiClient
-      .get<FetchGamesResonse>("/gamees", { signal: controller.signal })
+      .get<FetchGamesResonse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
