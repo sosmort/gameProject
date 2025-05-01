37185a268a559a8897d61c302e18f7346494ec29
sofiane <djebbsoso@gmail.com>
2025-05-01 15:34:07 +0100

    feat: show loading skeltons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,21 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "./hooks/useGames";
 import GameCards from "./GameCards";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, errors } = useGames();
+  const { games, errors, isLoading } = useGames();
+  const skeltons = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
   return (
     <>
       {errors && <Text>{errors}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
         spacing={10}
       >
+        {isLoading &&
+          skeltons.map((skelton) => <GameCardSkeleton key={skelton} />)}
         {games.map((game) => (
           <GameCards key={game.id} game={game} />
         ))}
