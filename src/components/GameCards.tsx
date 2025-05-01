2775a3fcb8e1f1156e886b95cc365347340e8156
sofiane <djebbsoso@gmail.com>
2025-05-01 15:10:36 +0100

    optimize images size by crop

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -2,13 +2,14 @@ import { Card, CardBody, Image, Heading, HStack } from "@chakra-ui/react";
 import { Game } from "./hooks/useGames";
 import PlatformIcons from "./PlatformIcons";
 import CriticScore from "./CriticScore";
+import getCroppedUrlImage from "../services/image-url";
 interface Props {
   game: Game;
 }
 const GameCards = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedUrlImage(game.background_image || "")} />
       <CardBody>
         <Heading fontSize="2xl">{game.name} </Heading>
         <HStack justifyContent="space-between">
