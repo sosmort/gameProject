dff49ea64235fb202a7e65f88a0da7e51fb92a94
sofiane <djebbsoso@gmail.com>
2025-05-05 10:03:21 +0100

    refactor: refactor: replace hardcoded skeleton array with dynamic Array.from method

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,17 +5,17 @@ import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { data, errors, isLoading } = useGames();
-  const skeltons = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
+  const skeletons = Array.from({ length: 15 });
   return (
     <>
       {errors && <Text>{errors}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={3}
+        spacing={5}
+        padding={5}
       >
         {isLoading &&
-          skeltons.map((skelton) => <GameCardSkeleton key={skelton} />)}
+          skeletons.map((_, index) => <GameCardSkeleton key={index} />)}
         {data.map((game) => (
           <GameCards key={game.id} game={game} />
         ))}
