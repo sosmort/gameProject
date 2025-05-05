33a5c81342e35ea3e4f8eca9dffb5a3e46c0cc91
sofiane <djebbsoso@gmail.com>
2025-05-05 15:58:35 +0100

    feat: show skelton text instead of genres when the page is loading

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,28 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Text,
+  SkeletonText,
+  SkeletonCircle,
+} from "@chakra-ui/react";
 import useGenres from "./hooks/useGenres";
 import getCroppedUrlImage from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
-
+  const { data, isLoading, errors } = useGenres();
+  const skeletons = Array.from({ length: 15 });
+  if (errors) return null;
   return (
     <List>
+      {isLoading &&
+        skeletons.map((_, index) => (
+          <HStack spacing="4" align="center" paddingY="5px">
+            <SkeletonCircle size="10" />
+            <SkeletonText noOfLines={2} spacing="4" width="80%" />
+          </HStack>
+        ))}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
