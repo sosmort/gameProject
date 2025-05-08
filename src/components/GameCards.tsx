70448d1a2d63201677083b0763d095033caa8a80
sofiane <djebbsoso@gmail.com>
2025-05-08 18:55:07 +0100

    add emptystate image to games without url image

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -3,13 +3,20 @@ import { Game, boxStyle } from "./hooks/useGames";
 import PlatformIcons from "./PlatformIcons";
 import CriticScore from "./CriticScore";
 import getCroppedUrlImage from "../services/image-url";
+import nullImage from "../assets/no-image-placeholder.webp";
 interface Props {
   game: Game;
 }
 const GameCards = ({ game }: Props) => {
   return (
     <Card style={boxStyle}>
-      <Image src={getCroppedUrlImage(game.background_image || "")} />
+      <Image
+        src={
+          getCroppedUrlImage(game.background_image || "")
+            ? getCroppedUrlImage(game.background_image || "")
+            : nullImage
+        }
+      />
       <CardBody>
         <Heading fontSize="2xl">{game.name} </Heading>
         <HStack justifyContent="space-between">
